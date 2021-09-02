9fd1c9dc3547d6b613a32ce70f39d59656b418fc
Drazy <kexin19991018@126.com>
2021-09-02 11:09:12 +0800

    🔧 增加 jsx 使用的配置

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
+/*
+ * @Author: Drazy
+ * @Date: 2021-09-01 16:23:32
+ * @LastEditors: Drazy
+ * @LastEditTime: 2021-09-02 11:07:22
+ * @Description: 配置 jsx 的使用
+ */
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"],
+  presets: [
+    [
+      "@vue/cli-plugin-babel/preset",
+      {
+        jsx: {
+          compositionAPI: true,
+          injectH: true,
+        },
+        module: false,
+      },
+    ],
+  ],
 };
