aa1e8acd902e5544305b5617318e44c27a179b2f
Drazy <kexin19991018@126.com>
2021-09-02 11:10:31 +0800

    ✨ 添加 MyElButton 的使用

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,23 +2,27 @@
  * @Author: Drazy
  * @Date: 2021-09-01 16:23:32
  * @LastEditors: Drazy
- * @LastEditTime: 2021-09-01 18:00:32
+ * @LastEditTime: 2021-09-02 11:08:03
  * @Description: 封装的组件
 -->
 
 <script>
 import { defineComponent, onMounted } from "@vue/composition-api";
+import { useElButton } from "../components/particles/MyElButton";
 export default defineComponent({
   setup() {
+    const primaryButton = useElButton({
+      text: "默认按钮",
+      onClick: () => {
+        alert("我被触发了~");
+      },
+    });
     onMounted(() => {
       console.log("mounted");
     });
-  },
-  render() {
-    return (
+    return () => (
       <div>
-        <div>123</div>
-        
+        <primaryButton.template></primaryButton.template>
       </div>
     );
   },
